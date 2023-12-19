fa34ecf10b5d2edbd3f591c2b556df51f5f4ab53
ywen <xie590206@gmail.com>
2023-12-19 14:53:17 +0100

    update

--- new/src/object_tracker/object_tracker/object_tracker.py
+++ new/src/object_tracker/object_tracker/object_tracker.py
@@ -27,24 +27,39 @@ class ObjectTracker(Node):
         self.subscription
         self._last_scan_time = time.time()
         self.last_position =None
-        self.last_time = None
+        self.last_time = self.get_clock().now()
 
        
-
+    def filter(self,msg):
+        for x in msg.ranges:
+            if x < 0.05:
+                msg.ranges.remove(x)
+        return msg
     
 
     def callback_laser_scan(self,msg):
         object_position = self.detect_object_position(msg)
         current_time = self.get_clock().now()
-        
+        valid_change_positions = []
         if self.last_position is not None and self.last_time is not None:
             change_positions = np.array(object_position) - np.array(self.last_position)
 
             time_interval = (current_time - self.last_time).nanoseconds / 1e9
 
-            velocity = change_positions / time_interval
-
-            self.get_logger().info(f'the velocity of object is: {velocity} m/s')
+            self.get_logger().info(f'the velocity of object is: {change_positions.shape} m/s')
+            for change_position in change_positions:
+                if np.linalg.norm(change_position) > 1e-4:
+                    #valid_change_positions= np.vstack((valid_change_positions,change_position))
+                    valid_change_positions.append(change_position)
+                else:
+                    #valid_change_positions= np.vstack((valid_change_positions,np.zeros(1,2)))
+                    valid_change_positions.append((0,0))
+            self.get_logger().info(f'the valid change_position is: {valid_change_positions} m/s')
+            velocity = ...
+            [(valid_change_position[0]/ time_interval, 
+              valid_change_position[1]/ time_interval) for valid_change_position in valid_change_positions]
+                       
+            self.get_logger().info(f'the velocity of object is: {np.linalg.norm(velocity)} m/s')
             #publish
             self.publisher_object_tracker(object_position,velocity)
 
@@ -74,7 +89,7 @@ class ObjectTracker(Node):
         
         distance_tax = [(np.linalg.norm(position), position) for position in  object_positions]
         distance_tax.sort(key=lambda x:x[0], reverse=True)
-        top_positions = np.array([pair[1] for pair in distance_tax[0:5]])  #select top five distances and position
+        top_positions = np.array([pair[1] for pair in distance_tax[0:2]])  #select top five distances and position
 
         
         return top_positions
@@ -82,12 +97,12 @@ class ObjectTracker(Node):
     def publisher_object_tracker(self,object_positions,velocitys):
         object_positions = np.array(object_positions)
         velocitys = np.array(velocitys)
-        combined_data = np.column_stack((object_positions[:, 0], velocitys[:, 0], object_positions[:, 1], velocitys[:, 1]))
+        combined_data = np.hstack((object_positions[:, [0]], velocitys[:, [0]], object_positions[:, [1]], velocitys[:, [1]]))
         flat_data = combined_data.flatten() #?
 
         pos_vel_msg = Float64MultiArray()
         pos_vel_msg.layout.dim = [
-            MultiArrayDimension(label='rows', size=5, stride=4*5),
+            MultiArrayDimension(label='rows', size=2, stride=4*2),
             MultiArrayDimension(label='columns', size=4, stride=4)
             
         ]
@@ -100,12 +115,12 @@ class ObjectTracker(Node):
 
     #def calc_verlocity(self,change_positions,time_interval):
     #   delta_time = time.time() - self._last_scan_time
-     #   self._last_scan_time = time.time()
+    #   self._last_scan_time = time.time()
 
     #    if delta_time < 1.0:
     #        return change_positions / time_interval
     #    else:
-   #         self.get_logger().warning("Time Step longer than 1 sec " + str(delta_time))
+    #         self.get_logger().warning("Time Step longer than 1 sec " + str(delta_time))
     
 def main(args=None):
     rclpy.init(args=args)
@@ -116,4 +131,5 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
 
